cfd92a7e79e0c951ce01990bc6dbd094d75f7790
gs195 <timedilation01@gmail.com>
2019-06-04 16:34:51 +0800

    react-fruit-basket-Save2

--- app.js
+++ app.js
@@ -8,10 +8,41 @@ class Basket extends React.Component {
   }
 
   handleChange(event) {
+    console.log("The initial stage is: " + this.state.value);
     this.setState({ value: event.target.value });
+    console.log("The updated stage is: " + this.state.value);
   }
 
+  filterFruits = () => {
+    return fruits.filter(fruit =>
+      fruit.includes(this.state.value.toLowerCase())
+    );
+  };
+
   render() {
-    return <input type="text" onChange={this.handleChange} />;
+    const fruitList = this.filterFruits().map(fruit => {
+      return <Item fruitName={fruit} />;
+    });
+
+    return (
+      <div id="container">
+        <input
+          type="text"
+          value={this.state.value}
+          onChange={this.handleChange}
+        />
+        <ul>{fruitList}</ul>
+      </div>
+    );
   }
 }
+
+function Item({ fruitName }) {
+  return <li>{fruitName}</li>;
+}
+
+const element = <Basket />;
+
+const container = document.querySelector("#app");
+
+ReactDOM.render(element, container);
