5e2ba8870516a636997ad779344652c145057d89
gs195 <timedilation01@gmail.com>
2019-06-06 09:32:27 +0800

    react-fruit-basket-Save4

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
-const fruits = ["apples", "oranges", "mangoes", "watermelons", "pears", "figs", "pineapples", "kiwi", "passion fruit", "black grapes", "green grapes", "plums"];
+const fruits = [
+  "apples",
+  "oranges",
+  "mangoes",
+  "watermelons",
+  "pears",
+  "figs",
+  "pineapples",
+  "kiwi",
+  "passion fruit",
+  "green grapes",
+  "plums"
+];
 
 class Basket extends React.Component {
   constructor(props) {
@@ -13,12 +25,13 @@ class Basket extends React.Component {
     //console.log("The updated stage is: " + this.state.value);
   }
 
-  filterFruits = () => { //
+  filterFruits = () => {
+    //
     let a = fruits.filter(fruit =>
       fruit.includes(this.state.value.toLowerCase())
     );
     console.log(a);
-    return a
+    return a;
   };
 
   render() {
@@ -28,13 +41,19 @@ class Basket extends React.Component {
       return <Item fruitName={fruit} />;
     });
 
-console.log("fruitList is: " + fruitList);
+    console.log("fruitList is: " + fruitList);
 
-    return ( //last step: put list of component tags in <ul> or <react.fragment>
+    return (
+      //last step: put list of component tags in <ul> or <react.fragment>
       //to display all the tags there are in the dom tree. when not list but
       //only 1 tag, directly insert it here without getting fruitList
       <div id="container">
-        <Input type="text" value={this.state.value} onChange={this.handleChange} placeholder="Search in fruit basket..."/>
+        <Input
+          type="text"
+          value={this.state.value}
+          onChange={this.handleChange}
+          placeholder="Search in fruit basket..."
+        />
         {/* <input
           type="text"
           value={this.state.value}
@@ -46,15 +65,21 @@ console.log("fruitList is: " + fruitList);
   }
 }
 
-
 //component tags are converted into html tags through
 //the following function
 function Item({ fruitName }) {
   return <li>{fruitName}</li>;
 }
 
-function Input({type, value, onChange, placeholder}) {
-  return (<input type={type} value={value} onChange={onChange} placeholder={placeholder}/>);
+function Input({ type, value, onChange, placeholder }) {
+  return (
+    <input
+      type={type}
+      value={value}
+      onChange={onChange}
+      placeholder={placeholder}
+    />
+  );
 }
 
 const element = <Basket />;
