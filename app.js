9f99d06b08e6196c6705c1cdff918ef2ca0c74d7
gs195 <timedilation01@gmail.com>
2019-06-06 15:57:04 +0800

    commit-app.js

--- app.js
+++ app.js
@@ -1,23 +1,80 @@
-const fruits = [
-  "apples",
-  "oranges",
-  "mangoes",
-  "watermelons",
-  "pears",
-  "figs",
-  "pineapples",
-  "kiwi",
-  "passion fruit",
-  "green grapes",
-  "plums"
-];
+
+// const fruits = [
+//   "apples",
+//   "oranges",
+//   "mangoes",
+//   "watermelons",
+//   "pears",
+//   "figs",
+//   "pineapples",
+//   "kiwi",
+//   "passion fruit",
+//   "green grapes",
+//   "plums"
+// ];
 
 class Basket extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { value: "" };
+    this.state = { value: "", fruits: [] };
     this.handleChange = this.handleChange.bind(this);
+    this.url =
+      "https://my-json-server.typicode.com/thoughtworks-jumpstart/api/fruits/";
   }
+
+  // async componentDidMount() {
+  //   try {
+  //     const response = await fetch(this.url);
+
+  //     if (!response.ok) {
+  //       throw new Error("Something happened");
+  //     }
+
+  //     const fruits = await response.json();
+  //     this.setState({ fruits });
+  //   } catch (err) {
+  //     console.error(err);
+  //   }
+  // }
+
+  componentDidMount() {
+    fetch(this.url)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("Something happened");
+        }
+        return response.json();
+      })
+      .then(data => {
+        this.setState(
+          {fruits:data}
+        );
+      });
+    console.log(this.state.fruits);
+  }
+
+  // componentDidMount() {
+  //   const url =
+  //     "https://my-json-server.typicode.com/thoughtworks-jumpstart/api/fruits/";
+
+  //   fetch(url)
+  //     .then(response => {
+  //       if (response.status !== 200) {
+  //         throw new Error("Ouch! This failed");
+  //       }
+  //       console.log("response", response);
+  //       return response.json();
+  //     })
+  //     .then(data => {
+  //       console.log("data: ", data);
+  //       this.setState({
+  //         fruits: data
+  //       });
+  //     })
+  //     .catch(err => {
+  //       console.log("this is an error", err.message);
+  //     });
+  // }
 
   handleChange(event) {
     //console.log("The initial stage is: " + this.state.value);
@@ -27,18 +84,18 @@ class Basket extends React.Component {
 
   filterFruits = () => {
     //
-    let a = fruits.filter(fruit =>
-      fruit.includes(this.state.value.toLowerCase())
+    let nameList = this.state.fruits.filter(fruit =>
+      fruit.type.includes(this.state.value.toLowerCase())
     );
-    console.log(a);
-    return a;
+    console.log("nameList:",nameList);
+    return nameList;
   };
 
   render() {
     //use map function to convert array/object literal into component
     //tag with attributes obtained from the array/object literal
     const fruitList = this.filterFruits().map(fruit => {
-      return <Item fruitName={fruit} />;
+      return <Item fruitEmoji={fruit.emoji} fruitName={fruit.type} />;
     });
 
     console.log("fruitList is: " + fruitList);
@@ -67,8 +124,8 @@ class Basket extends React.Component {
 
 //component tags are converted into html tags through
 //the following function
-function Item({ fruitName }) {
-  return <li>{fruitName}</li>;
+function Item({ fruitName, fruitEmoji }) {
+  return <li>{fruitEmoji} {fruitName}</li>;
 }
 
 function Input({ type, value, onChange, placeholder }) {
